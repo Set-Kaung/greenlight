5ea5b46409ee6c8415522ac321ac2430d50ad9ae
Set-Kaung <zetaprotocol8@gmail.com>
2024-05-08 21:21:02 +0700

    add movie details update feature

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -7,5 +7,6 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /v1/healthcheck", app.healthcheck)
 	mux.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
 	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
+	mux.HandleFunc("PUT /v1/movies/{id}", app.updateMovieHandler)
 	return mux
 }
